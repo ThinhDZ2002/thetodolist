264c9d9e251da2ae696f37b2100c3d12e0ac3455
tranv <tranvanthinh20072002@gmail.com>
2024-11-14 11:40:11 +0700

    upload code

---
+++ lesson1/main.js
@@ -0,0 +1,14 @@
+
+// var fullName = "Tran Van Thinh";
+// var age=22;
+// alert(fullName);
+ console.log(2**5);
+// confirm("Xac nhan ban du tuoi!");
+// prompt("Loi ban gap phai la gi?");
+// setTimeout(function() {
+//     alert("Thong bao sau 3s");
+// },3000);
+// setInterval(function() {
+//     console.log("Day la log sau moi 3s"+ Math.random());
+// },3000);
+
